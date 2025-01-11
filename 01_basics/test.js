0cda2bbf343be5dc79123feac3a5cfca7114394b
prapti-chavan <71283167+prapti3@users.noreply.github.com>
2025-01-11 13:50:25 +0000

    update done

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,15 @@
-console.log("Hello This is JS");
+// const name = "PRAPTI"
+// let surname = "CHAVAV"
+// const number = 9307162558
+// let account;
+
+// console.table([name,surname,number,account])
+
+// console.log(typeof undefined)
+// console.log(typeof null)
+
+let n = 1;
+console.log(typeof n);
+
+let num = Number(n);
+console.log(typeof num);
